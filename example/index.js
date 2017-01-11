b6ed51417795c3f9c7a1e6c3ffdbc9f5accf012c
Christian Froehlingsdorf <christian.froehlingsdorf@rewe-digital.com>
2017-01-11 16:12:38 +0100

    added possibility to re-connection subscriptions periodically

--- example/index.js
+++ example/index.js
@@ -2,6 +2,7 @@ const Pohl = require("./../index.js");
 
 const config = {
     topic: "pohl",
+    autoReconnectInterval: 1500,
     cache: {
         redis: {
             port: 6379,
@@ -23,7 +24,10 @@ const config = {
 //of course your tasks can differ, and you can return errors, just like
 //you use the old callbacks
 const p = new Pohl(config);
+const p2 = new Pohl(config);
+const p3 = new Pohl(config);
 
+/*
 //listen for some metric events (to send to influx for example)
 p.on("metric", (type, value) => {
     switch(type){
@@ -31,7 +35,7 @@ p.on("metric", (type, value) => {
         case "send": console.log("send state increment for: " + value); break;
         default: console.log(type + ": " + value); break;
     }
-});
+}); */
 
 //on one service you setup a receiver, that works on tasks and returns them back to the sender
 
@@ -51,9 +55,19 @@ p.setupTaskReceiver(receiveTask, () => {
     console.log("instance ready to receive tasks.");
 });
 
+p2.setupTaskReceiver(receiveTask, () => {
+    console.log("instance ready to receive tasks.");
+});
+
+p3.setupTaskReceiver(receiveTask, () => {
+    console.log("instance ready to receive tasks.");
+});
+
 //on the other service you setup a sender, that sends tasks and gets a callback when the receiver sends a result
 
-setTimeout(() => {
+let sendCount = 0;
+
+setInterval(() => {
 
     let aTask = {
         make: "something",
@@ -61,6 +75,7 @@ setTimeout(() => {
     };
 
     let startT = Date.now();
+    sendCount++;
 
     p.sendTask(aTask,
         (err, taskResult) => { //** this will be called :D *magic*
@@ -72,38 +87,12 @@ setTimeout(() => {
             }
 
            console.log(taskResult);
-            console.log("took: " + (endT - startT) + "ms.");
+            console.log("took: " + (endT - startT) + "ms. -> " + sendCount);
         }
     );
 
-}, 100);
-
-//again for the time (needs to warm up)
-setTimeout(() => {
-
-    let aTask = {
-        make: "something",
-        with: "this thing"
-    };
-
-    let startT = Date.now();
-
-    p.sendTask(aTask,
-        (err, taskResult) => { //** this will be called :D *magic*
-
-            let endT = Date.now();
-
-            if(err){
-                return console.log(err);
-            }
-
-            console.log(taskResult);
-            console.log("took: " + (endT - startT) + "ms.");
-        }
-    );
-
-}, 150);
+}, 3000);
 
 setTimeout(() => {
-     process.exit(0);
+     //process.exit(0);
 }, 300);
